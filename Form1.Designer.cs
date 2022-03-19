3a4e681c3ea21dc48df48e9b4b3ca36ff41e10dc
RaneemOb <88738431+RaneemOb@users.noreply.github.com>
2022-03-19 22:36:43 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,187 @@
+﻿
+namespace Assi2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.actionToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.sTToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.sTOPGAMEToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.shapeContainer1 = new Microsoft.VisualBasic.PowerPacks.ShapeContainer();
+            this.ovalShape3 = new Microsoft.VisualBasic.PowerPacks.OvalShape();
+            this.ovalShape2 = new Microsoft.VisualBasic.PowerPacks.OvalShape();
+            this.ovalShape1 = new Microsoft.VisualBasic.PowerPacks.OvalShape();
+            this.rectangleShape1 = new Microsoft.VisualBasic.PowerPacks.RectangleShape();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.actionToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(798, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // actionToolStripMenuItem
+            // 
+            this.actionToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.sTToolStripMenuItem,
+            this.sTOPGAMEToolStripMenuItem});
+            this.actionToolStripMenuItem.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.actionToolStripMenuItem.Name = "actionToolStripMenuItem";
+            this.actionToolStripMenuItem.Size = new System.Drawing.Size(55, 20);
+            this.actionToolStripMenuItem.Text = "Action";
+            this.actionToolStripMenuItem.Click += new System.EventHandler(this.actionToolStripMenuItem_Click);
+            // 
+            // sTToolStripMenuItem
+            // 
+            this.sTToolStripMenuItem.Name = "sTToolStripMenuItem";
+            this.sTToolStripMenuItem.Size = new System.Drawing.Size(147, 22);
+            this.sTToolStripMenuItem.Text = "START GAME";
+            this.sTToolStripMenuItem.Click += new System.EventHandler(this.sTToolStripMenuItem_Click);
+            // 
+            // sTOPGAMEToolStripMenuItem
+            // 
+            this.sTOPGAMEToolStripMenuItem.Name = "sTOPGAMEToolStripMenuItem";
+            this.sTOPGAMEToolStripMenuItem.Size = new System.Drawing.Size(147, 22);
+            this.sTOPGAMEToolStripMenuItem.Text = "STOP GAME";
+            this.sTOPGAMEToolStripMenuItem.Click += new System.EventHandler(this.sTOPGAMEToolStripMenuItem_Click);
+            // 
+            // shapeContainer1
+            // 
+            this.shapeContainer1.Location = new System.Drawing.Point(0, 0);
+            this.shapeContainer1.Margin = new System.Windows.Forms.Padding(0);
+            this.shapeContainer1.Name = "shapeContainer1";
+            this.shapeContainer1.Shapes.AddRange(new Microsoft.VisualBasic.PowerPacks.Shape[] {
+            this.ovalShape3,
+            this.ovalShape2,
+            this.ovalShape1,
+            this.rectangleShape1});
+            this.shapeContainer1.Size = new System.Drawing.Size(798, 450);
+            this.shapeContainer1.TabIndex = 1;
+            this.shapeContainer1.TabStop = false;
+            // 
+            // ovalShape3
+            // 
+            this.ovalShape3.BackColor = System.Drawing.Color.Green;
+            this.ovalShape3.BackStyle = Microsoft.VisualBasic.PowerPacks.BackStyle.Opaque;
+            this.ovalShape3.Location = new System.Drawing.Point(694, 319);
+            this.ovalShape3.Name = "ovalShape3";
+            this.ovalShape3.Size = new System.Drawing.Size(83, 81);
+            this.ovalShape3.Click += new System.EventHandler(this.ovalShape3_Click);
+            // 
+            // ovalShape2
+            // 
+            this.ovalShape2.BackColor = System.Drawing.Color.Yellow;
+            this.ovalShape2.BackStyle = Microsoft.VisualBasic.PowerPacks.BackStyle.Opaque;
+            this.ovalShape2.Location = new System.Drawing.Point(696, 181);
+            this.ovalShape2.Name = "ovalShape2";
+            this.ovalShape2.Size = new System.Drawing.Size(81, 80);
+            this.ovalShape2.Click += new System.EventHandler(this.ovalShape2_Click);
+            // 
+            // ovalShape1
+            // 
+            this.ovalShape1.BackColor = System.Drawing.Color.Red;
+            this.ovalShape1.BackStyle = Microsoft.VisualBasic.PowerPacks.BackStyle.Opaque;
+            this.ovalShape1.FillGradientColor = System.Drawing.Color.Red;
+            this.ovalShape1.Location = new System.Drawing.Point(694, 54);
+            this.ovalShape1.Name = "ovalShape1";
+            this.ovalShape1.Size = new System.Drawing.Size(83, 72);
+            this.ovalShape1.Click += new System.EventHandler(this.ovalShape1_Click);
+            // 
+            // rectangleShape1
+            // 
+            this.rectangleShape1.BackColor = System.Drawing.Color.Black;
+            this.rectangleShape1.BorderColor = System.Drawing.Color.Black;
+            this.rectangleShape1.FillColor = System.Drawing.Color.Black;
+            this.rectangleShape1.FillGradientColor = System.Drawing.Color.Black;
+            this.rectangleShape1.FillStyle = Microsoft.VisualBasic.PowerPacks.FillStyle.Solid;
+            this.rectangleShape1.Location = new System.Drawing.Point(667, 24);
+            this.rectangleShape1.Name = "rectangleShape1";
+            this.rectangleShape1.SelectionColor = System.Drawing.Color.Black;
+            this.rectangleShape1.Size = new System.Drawing.Size(131, 425);
+            this.rectangleShape1.Click += new System.EventHandler(this.rectangleShape1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Assi2.Resource1.CAR;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 134);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(109, 76);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 2000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(798, 450);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.Controls.Add(this.shapeContainer1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private Microsoft.VisualBasic.PowerPacks.ShapeContainer shapeContainer1;
+        private Microsoft.VisualBasic.PowerPacks.RectangleShape rectangleShape1;
+        private Microsoft.VisualBasic.PowerPacks.OvalShape ovalShape1;
+        private Microsoft.VisualBasic.PowerPacks.OvalShape ovalShape3;
+        private Microsoft.VisualBasic.PowerPacks.OvalShape ovalShape2;
+        private System.Windows.Forms.ToolStripMenuItem actionToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem sTToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem sTOPGAMEToolStripMenuItem;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using Microsoft.VisualBasic.PowerPacks;
+
+namespace Assi2
+{
+    public partial class Form1 : Form
+    {
+        string location;
+        public Form1()
+        {
+            InitializeComponent();
+            pictureBox1.Visible = false;
+
+        }
+
+        private void rectangleShape1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ovalShape1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ovalShape2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ovalShape3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void actionToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+        int x;
+        int y;
+        private void sTToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dlg = new OpenFileDialog();
+            dlg.Filter = "TextFile|*.txt|XMLFILE|*.xml";
+            if (dlg.ShowDialog() == DialogResult.OK)
+            {
+                if (dlg.FilterIndex == 1)
+                {
+                    StreamReader Reader = new StreamReader(dlg.FileName);
+                    while (!Reader.EndOfStream)
+                    {
+
+                        location = Reader.ReadLine();
+
+                    }
+
+                    Reader.Close();
+                }
+            }
+            string[] numbers = location.Split(new char[] { '(', ',', ')' }, StringSplitOptions.RemoveEmptyEntries);
+
+            for (int i = 0; i < numbers.Length; i += 2)
+            {
+                x = Convert.ToInt32(numbers[i]);
+                y = Convert.ToInt32(numbers[i + 1]);
+            }
+            pictureBox1.Location = new Point(x, y + menuStrip1.Height);
+            pictureBox1.Visible = true;
+            timer1.Enabled = true;
+        }
+
+        private void sTOPGAMEToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = false;
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            int x = pictureBox1.Location.X;
+            int y = pictureBox1.Location.Y;
+
+
+            if (ovalShape1.Visible)
+            {
+                pictureBox1.Location = new Point(pictureBox1.Location.X, pictureBox1.Location.Y);
+                ovalShape1.Visible = false;
+                ovalShape2.Visible = false;
+                ovalShape3.Visible = true;
+
+
+
+            }
+            else if (ovalShape3.Visible)
+            {
+                ovalShape1.Visible = false;
+                ovalShape2.Visible = true;
+                ovalShape3.Visible = false;
+                x = pictureBox1.Location.X + 150;
+
+                pictureBox1.Location = new Point(x, y);
+                if (pictureBox1.Location.X + pictureBox1.Width > this.Width - rectangleShape1.Width)
+                    pictureBox1.Location = new Point(0, y);
+
+            }
+            else if (ovalShape2.Visible)
+            {
+                ovalShape1.Visible = true;
+                ovalShape2.Visible = false;
+                ovalShape3.Visible = false;
+
+                x = pictureBox1.Location.X + 5;
+
+                pictureBox1.Location = new Point(x, y);
+                if (pictureBox1.Location.X + pictureBox1.Width > this.Width - rectangleShape1.Width)
+                    pictureBox1.Location = new Point(0, y);
+            }
+
+
+
+
+
+        }
+    }
+}
